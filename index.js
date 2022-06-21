c114026ad252a05f7ef73dee8fd59059e431d1db
Vishnu Kumar PS <vishnu.kumarps@claysys.net>
2022-06-21 14:03:14 +0530

    sample code

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-jhjhfd
+const express = require('express')
+const app = express()
+const port = 3000
+
+app.get('/', (req, res) => res.send('Hello World!'))
+
+
+app.get('/api/v1/users', (req, res, next) => {
+    console.log('users')
+
+    res.send('Done');
+});
+app.listen(port, () => console.log(`Example app listening on port ${port}!`))
